e291773a0c5afff7b280a58448f3ce51d1282563
Owen O'Donnell <oodonne@tcd.ie>
2021-05-09 15:46:31 +0100

    station summary partial introduced and changes to Dashboard and Dasboard controller implemented

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -8,6 +8,7 @@ import models.Station ;
 import models.Reading;
 import play.Logger;
 import play.mvc.Controller;
+import utils.StationAnalytics;
 
 public class Dashboard extends Controller
 {
@@ -15,6 +16,29 @@ public class Dashboard extends Controller
     Logger.info("Rendering Dashboard");
     Member member = Accounts.getLoggedInMember();
     List<Station> stations = member.stations;
+    for (Station station: stations) {
+      if (station.readings.size()>0) {
+        Reading latestReading = station.readings.get(station.readings.size() - 1);
+        station.tempCelsius = latestReading.temperature;
+        station.pressure = latestReading.pressure;
+        station.weatherConditions = StationAnalytics.codeToWeatherConditions(latestReading.code);
+        station.tempFarenheit = StationAnalytics.celsiusToFahrenheit(latestReading.temperature);
+        station.beaufort = StationAnalytics.kmToBeaufort(latestReading.windSpeed);
+        station.compassDirection = StationAnalytics.degreesToDirection(latestReading.windDirection);
+        if ((latestReading.temperature<=10) && (latestReading.windSpeed>4.8)) {
+          station.windChill = StationAnalytics.windChillCalculator(latestReading.temperature, latestReading.windSpeed);
+        } else {
+          station.windChill = station.tempCelsius;
+        }
+        station.weatherIcon = StationAnalytics.codeToWeatherIcons(latestReading.code);
+        station.minWindSpeed = StationAnalytics.minWindSpeed(station.readings);
+        station.maxWindSpeed = StationAnalytics.maxWindSpeed(station.readings);
+        station.maxPressure = StationAnalytics.maxPressure(station.readings);
+        station.minPressure = StationAnalytics.minPressure(station.readings);
+        station.maxTemp = StationAnalytics.maxTemp(station.readings);
+        station.minTemp = StationAnalytics.minTemp(station.readings);
+      }
+    }
     render ("dashboard.html", stations);
   }
 
